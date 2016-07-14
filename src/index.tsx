5d067c470987926ef5ff61d3c42056a5dbb5cf38
mrandri19 <andrecogno@hotmail.it>
2016-07-14 23:31:07 +0200

    First react+redux+typescript implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,45 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 
-import { Hello } from "./components/Hello";
+type AppState = {
+    username: string
+}
+
+type HelloProps = {
+    todos: AppState
+};
+
+const initialState: AppState = {
+    username: 'Andrea'
+}
+
+function reducer(state = initialState, action: any) {
+    return state;
+};
+
+let store = createStore(reducer);
+
+function Hello(props: HelloProps) {
+    return <h1>{props.todos.username}</h1>;
+}
+
+function mapDispatchToProps(dispatch: any) {
+    // Every time the state is updated the props are recalculated
+    return {};
+}
+
+function mapStateToProps(state: AppState): HelloProps {
+    // Every time the state is updated the props are recalculated
+    return {todos: state};
+};
+
+let App = connect(mapStateToProps, mapDispatchToProps)(Hello);
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("example")
 );
