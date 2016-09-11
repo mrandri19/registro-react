ab03954b7befe6c34a8c8459b9caf76a4987a87f
mrandri19 <andrecogno@hotmail.it>
2016-09-11 16:36:03 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo "Starting"
 
-echo "Getting dependencies"
-npm i --dev
+# echo "Getting dependencies"
+# npm i --dev
 
-echo "Getting typings"
-typings i
+# echo "Getting typings"
+# typings i
 
 echo "Building production release"
 npm run build-prod
 
 echo "Building dockerfile"
-docker 
+docker build -t mrandri19/registro-react:latest .
+
+echo "Pushing dockerfile"
+docker push mrandri19/registro-react:latest
