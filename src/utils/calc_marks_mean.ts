14f5edf8def9e0ea94618054ca70571c7a61e61b
mrandri19 <andrecogno@hotmail.it>
2016-11-02 21:11:45 +0100

    Server now sends numbers instead of strings, update mean calculation

--- src/utils/calc_marks_mean.ts
+++ src/utils/calc_marks_mean.ts
@@ -1,11 +1,9 @@
 import { Mark } from "../types";
-import { grade_to_float } from "./grade_to_float";
 
 export function calc_marks_mean(input: Mark[]): number {
     let firstSemesterMarks = input
-        .filter(sub => !sub.ns) // && !sub.ns filters out the blue grades which shouldn't count
-        .map(mark => grade_to_float(mark.mark))
-        .filter(mark => mark !== undefined);
+        .filter(sub => !sub.ns)
+        .map(mark => mark.mark) // && !sub.ns filters out the blue grades which shouldn't count
     let mean = firstSemesterMarks.reduce((acc, mark) => acc + mark, 0) / firstSemesterMarks.length;
     return mean;
 }
